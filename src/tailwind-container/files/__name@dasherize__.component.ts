b2be0b203c677a3f128d4a325fb177f8ec00f3f6
looegi <lc1512@humboldt.edu>
2023-02-14 22:28:42 -0800

    init

---
+++ src/tailwind-container/files/__name@dasherize__.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-<%= dasherize(name) %>',
+    templateUrl: './<%= dasherize(name) %>.component.html'
+})
+export class My<%= classify(name) %>Component {
+
+
+}
